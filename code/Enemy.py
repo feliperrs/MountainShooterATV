5f83fedaeb864db7398d08d366f263f1772a639d
Felipe Ribeiro Rodrigues dos Santos <feliperrsdev@gmail.com>
2025-05-25 23:41:32 -0300

    9.	(DIFÍCIL) O comportamento de movimento do ‘Enemy3’ na tela deve ser diferente dos demais inimigos. Este inimigo deve se mover da seguinte maneira: 	i.	No eixo horizontal (eixo x), ele deve se mover da direita para a esquerda; 	ii.	No eixo vertical (eixo y), ele deve ter um movimento implementado que faça com que ele fique sempre subindo e descendo na tela, de acordo com: 		1.	Quando ‘Enemy3’ bater na borda inferior da tela, ele deve começar a subir mantendo sua velocidade normal. 		2.	Quando ‘Enemy3’ bater na borda superior da tela, ele deve começar a descer com o dobro de sua velocidade.

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from code.Const import ENTITY_SPEED, ENTITY_SHOT_DELAY
+from code.Const import ENTITY_SPEED, ENTITY_SHOT_DELAY, WIN_HEIGHT
 from code.EnemyShot import EnemyShot
 from code.Entity import Entity
 
@@ -9,9 +9,24 @@ class Enemy(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
         self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+        self.moving_down = True
 
     def move(self):
-        self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.name == 'Enemy3':
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+            if self.moving_down:
+                self.rect.centery += ENTITY_SPEED[self.name] * 2
+                if self.rect.bottom >= WIN_HEIGHT:
+                    self.rect.bottom = WIN_HEIGHT
+                    self.moving_down = False
+            else:
+                self.rect.centery -= ENTITY_SPEED[self.name]
+                if self.rect.top <= 0:
+                    self.rect.top = 0
+                    self.moving_down = True
+                    
+        else:
+            self.rect.centerx -= ENTITY_SPEED[self.name]
 
     def shoot(self):
         self.shot_delay -= 1
